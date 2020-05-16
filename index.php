67b5f08ded2e5c82d286990d78c0e5f8b8e1d689
engj3ll <arberpr6@gmail.com>
2020-05-16 11:34:39 +0200

    arrays improved

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
               <div class="dropdown-content">
                 <a href="#">Fituesit e oskareve</a>
                 <a href="https://www.imdb.com/list/ls024149810/">Top 250</a>
-                <a href="#">Filmat me te ri</a>
+                <a href="comment.php">Reviews</a>
               </div>
             </div>
             <a>Faqja Kryesore</a>
